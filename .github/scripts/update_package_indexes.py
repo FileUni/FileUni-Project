260c5365c9405835b5bb42e2bc405881a0406499
FileUni <git@fileuni.com>
2026-03-10 17:58:36 +0800

    fix(nix): remove unfree license declaration from package
    The FileUni Community Source License is not in nixpkgs license list,
    so remove the license field to avoid evaluation warnings.

--- .github/scripts/update_package_indexes.py
+++ .github/scripts/update_package_indexes.py
@@ -227,7 +227,6 @@ stdenvNoCC.mkDerivation {{
     description = "FileUni CLI";
     homepage = "https://fileuni.com";
     mainProgram = "fileuni";
-    license = licenses.unfree;
     platforms = builtins.attrNames sources;
     sourceProvenance = with sourceTypes; [ binaryNativeCode ];
   }};
